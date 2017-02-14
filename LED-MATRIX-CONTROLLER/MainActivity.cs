31709cfa4f7416cec3389a74694054607510822d
OGBE OZIOMA JNR <ogbeoziomajnr@yahoo.com>
2017-02-14 10:17:49 +0100

    Adding action bar tabs

--- LED-MATRIX-CONTROLLER/MainActivity.cs
+++ LED-MATRIX-CONTROLLER/MainActivity.cs
@@ -15,6 +15,13 @@ namespace LEDMATRIXCONTROLLER
 
 			// Set our view from the "main" layout resource
 			SetContentView (Resource.Layout.Main);
+            ActionBar.SetHomeButtonEnabled(true);
+            ActionBar.NavigationMode = ActionBarNavigationMode.Tabs;
+
+            var tab1 = ActionBar.NewTab();
+            tab1.SetText("Settings");
+           // tab1.SetIcon(Resource.Mipmap.Icon);
+            ActionBar.Show();
 
 			// Get our button from the layout resource,
 			// and attach an event to it
@@ -24,9 +31,10 @@ namespace LEDMATRIXCONTROLLER
 
                 progress = new Android.App.ProgressDialog(this);
                 progress.Indeterminate = true;
-                progress.SetProgressStyle(Android.App.ProgressDialogStyle.Horizontal);
+                progress.SetProgressStyle(Android.App.ProgressDialogStyle.Spinner);
                 progress.SetMessage("Connecting");
                 progress.SetCancelable(true);
+                progress.SetTitle("Bleutooth");
                 progress.Show();
             };
 		}
